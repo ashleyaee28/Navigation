4a486bef80c5453343d85021277d126176e5a48d
Ashley Barrera <ashleyaee83@gmail.com>
2023-07-21 11:36:04 -0500

    ok

--- Navigation/ContentView.swift
+++ Navigation/ContentView.swift
@@ -9,18 +9,32 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        NavigationStack {
+            VStack(spacing: 4.0) {
+                Text("This is the root view")
+                
+                NavigationLink(destination: Text("You have arrived at your second view") ){
+                    Text("Click here")
+                } .toolbar {
+                    ToolbarItemGroup(placement: .status) {
+                        NavigationLink(destination:SecondView()) {
+                            Text("Click here")
+                        }
+                    }
+                }
+                .navigationTitle("Home")
+                .navigationBarTitleDisplayMode(.inline)
+                .navigationBarHidden(false)
+            }
+            
+            
         }
-        .padding()
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
